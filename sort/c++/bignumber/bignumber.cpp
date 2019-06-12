f9ef14e684022e464d06a6d35c5bad8af05add14
ParkJunYong <pjy1418@gmail.com>
2019-06-12 23:28:58 +0900

    init bignumber.cpp

---
+++ sort/c++/bignumber/bignumber.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int compare(int first_input,int second_input){
+  int ret = -1;
+  //가장 큰수를 만들수 있는 수인지 비교
+  
+  
+  return ret
+}
+
+string solution(vector<int> numbers) {
+
+  for(int i= 0  )
+    
+  
+    string answer = "";
+    return answer;
+}
+
+
+
+void main(){
+  
+
+}
