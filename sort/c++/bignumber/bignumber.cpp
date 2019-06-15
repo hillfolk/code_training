7982fa268c42c259871e596d702893e1a5a094d6
ParkJunYong <pjy1418@gmail.com>
2019-06-15 23:43:14 +0900

    fix compare

--- sort/c++/bignumber/bignumber.cpp
+++ sort/c++/bignumber/bignumber.cpp
@@ -1,15 +1,18 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-bool compare(int left,int rigiht){
+bool compare(int left,int right){
 
   string str_left =  to_string(left);
   string str_right = to_string(right);
 
-  int left_right = stoi(str_left + str_right);
-  int rigiht_left = stoi(str_rigiht + str_left);
+  int left_right =  stoi(str_left+str_right);
+  cout << left_right <<endl;
+  int right_left = stoi(str_right+str_left);
+  cout << right_left <<endl;
   
 
   if (left_right > right_left ){
@@ -21,20 +24,37 @@ bool compare(int left,int rigiht){
 }
 
 string solution(vector<int> numbers) {
-
-  // 1 비교
-  // 스왑
-  //정렬 완료후 문자열 변환 
-  sort(numbers.begin,numbers.end,compare)
-    
   
-    string answer = "";
-    return answer;
+  int zcnt = 0;
+  string result = "";
+  sort(numbers.begin(),numbers.end(),compare);
+
+  for (int i = 0; i < numbers.size(); ++i){
+    if (numbers[i] == 0){
+      zcnt ++;
+    }
+
+    result = result + to_string(numbers[i]);
+  
+  
+  }
+  if (zcnt == numbers.size()){
+    result = "0";
+  }
+  return result;
 }
 
 
+int main(){
+  vector<int> input_vector;
 
-void main(){
+  input_vector.push_back(6);
+  input_vector.push_back(10);
+  input_vector.push_back(2);
+    input_vector.push_back(3);
+
+        input_vector.push_back(30);
   
+  cout << solution(input_vector) << endl;
 
 }
