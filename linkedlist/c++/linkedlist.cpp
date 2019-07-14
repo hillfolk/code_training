9319841b687529ca45db015f7b934dd7609d1b69
hillfolk <pjy1418@gmail.com>
2019-07-15 00:22:12 +0900

    add: linkedlist.cpp

---
+++ linkedlist/c++/linkedlist.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+  Node *next = null;
+  int data;
+
+  pulbic Node(int d) {
+    data = d;
+  }
+
+  void appendToTail(int d){
+    Node *end = new Node(d);
+    Node *n = this;
+    while ( n.next != null){
+      n = n.next;
+    }
+
+    n.next = end;
+  }
+
+  Node deleteNode(Node head, int d){
+    Node *n = head;
+    if (n.data == d){
+      return head.next; 
+    }
+
+    while (n.next != null){
+      if (n.next.data == d) {
+	n.next = n.next.next;
+	return head;
+      }
+      n = n.next;
+    }
+    return head
+  }
+  
+}
+  
