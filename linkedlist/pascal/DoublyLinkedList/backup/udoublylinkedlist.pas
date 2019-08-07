8cbaee71f31f04573c4436799f9f25b6de5eb981
hillfolk <pjy1418@gmail.com>
2019-08-08 01:17:23 +0900

    fix duublyLinkedList

--- linkedlist/pascal/DoublyLinkedList/backup/udoublylinkedlist.pas
+++ linkedlist/pascal/DoublyLinkedList/backup/udoublylinkedlist.pas
@@ -18,10 +18,10 @@ type
     FValue: Pointer;
   public
     constructor Create;
-    function hasNext:Boolean;
-    function hasPrev:Boolean;
+    function hasNext: boolean;
+    function hasPrev: boolean;
     property Value: Pointer read FValue write FValue;
-    property Next: TNode read FNext write FPrev;
+    property Next: TNode read FNext write FNext;
     property Prev: TNode read FPrev write FPrev;
 
   end;
@@ -33,9 +33,9 @@ type
     FTail: TNode;
     FCount: integer;
   public
-    procedure InsertBefore(APosNode:TNode;AValuePtr:Pointer);
-    procedure InsertNext(APosNode:TNode;AValuePtr:Pointer);
-    procedure DeleteAt(APos:TNode);
+    procedure InsertBefore(APosNode: TNode; var AValuePtr: Pointer);
+    procedure InsertNext(APosNode: TNode; var AValuePtr: Pointer);
+    procedure DeleteAt(APos: TNode);
     property Head: TNode read FHead write FHead;
     property Tail: TNode read FTail write FTail;
     property Count: integer read FCount write FCount;
@@ -53,49 +53,102 @@ begin
 
 end;
 
-function TNode.hasNext: Boolean;
+function TNode.hasNext: boolean;
 begin
-     if Assigned(FNext) then Result := True;
-     else Result := False;
+  if Assigned(FNext) then
+  begin
+    Result := True;
+
+  end
+  else
+  begin
+    Result := False;
+
+  end;
 end;
 
-function TNode.hasPrev: Boolean;
+function TNode.hasPrev: boolean;
 begin
-       if Assigned(FPrev) then Result := True;
-     else Result := False;
+  if Assigned(FPrev) then
+  begin
+    Result := True;
+  end
+  else
+    Result := False;
 end;
 
 { TDoublyLinkedList }
 
-procedure TDoublyLinkedList.InsertBefore(APosNode: TNode; AValuePtr: Pointer);
+procedure TDoublyLinkedList.InsertBefore(APosNode: TNode; var AValuePtr: Pointer);
 var
-  LNode:TNode;
+  LNode: TNode;
 begin
+  if not Assigned(APosNode) then
+  begin
+    Head := TNode.Create;
+    Head.Value := AValuePtr;
+    exit;
+  end;
 
-     if Assigned(APosNode.Prev) then
-     begin
-     LNode :=  APosNode.Prev ;
-     APosNode.Prev := TNode.Create;
-     APosNode.Prev.Value := AValuePtr;
-     APosNode.Prev.Prev := LNode;
-     end;
-
+  if Assigned(APosNode.Prev) then
+  begin
+    LNode := APosNode.Prev;
+    APosNode.Prev := TNode.Create;
+    APosNode.Prev.Value := AValuePtr;
+    APosNode.Prev.Prev := LNode;
+  end
+  else
+  begin
+    APosNode.Prev := TNode.Create;
+    APosNode.Prev.Value := AValuePtr;
+    Head := APosNode.Prev;
+  end;
+  Inc(FCount);
 end;
 
-procedure TDoublyLinkedList.InsertNext(APosNode: TNode; AValuePtr: Pointer);
+procedure TDoublyLinkedList.InsertNext(APosNode: TNode; var AValuePtr: Pointer);
+var
+  LNode: TNode;
 begin
-         if Assigned(APosNode.Prev) then
-     begin
-     LNode :=  APosNode.Next ;
-     APosNode.Next := TNode.Create;
-     APosNode.Next.Value := AValuePtr;
-     APosNode.Next.Prev := LNode;
-     end;
+  if not Assigned(APosNode) then
+  begin
+    Tail := TNode.Create;
+    Tail.Value := AValuePtr;
+    exit;
+  end;
+  if Assigned(APosNode.Next) then
+  begin
+    LNode := APosNode.Next;
+    APosNode.Next := TNode.Create;
+    APosNode.Next.Value := AValuePtr;
+    APosNode.Next.Prev := LNode;
+  end
+  else
+  begin
+
+    APosNode.Next := TNode.Create;
+    APosNode.Next.Value := AValuePtr;
+    Tail := APosNode.Next;
+  end;
+  Inc(FCount);
 
 end;
 
 procedure TDoublyLinkedList.DeleteAt(APos: TNode);
+var
+  LNode: TNode;
 begin
+  if Assigned(APos.Prev) then
+  begin
+    LNode := APos.Prev;
+    LNode.Next := APos.Next;
+    FreeAndNil(APos);
+  end
+  else
+  begin
+    FreeAndNil(APos);
+  end;
+  Dec(FCount);
 
 end;
 
@@ -106,7 +159,6 @@ begin
   FHead := nil;
   FTail := nil;
   FCount := 0;
-  //
 
 end;
 
--- linkedlist/pascal/DoublyLinkedList/doublelinkedlist.pas
+++ linkedlist/pascal/DoublyLinkedList/doublelinkedlist.pas
@@ -9,7 +9,36 @@ uses
   Classes, uDoublyLinkedList;
   { you can add units after this }
 
+var
+  LinkedList:TDoublyLinkedList;
+  LStr1,LStr2:String;
+  LStrPtr:^String;
+  LNode,LCurrentNode:TNode;
 begin
+  LinkedList := TDoublyLinkedList.Create;
+  New(LStrPtr);
+  LStrPtr^ := 'Head';
+  LinkedList.InsertBefore(nil,LStrPtr);
+
+  New(LStrPtr);
+  LStrPtr^ := 'Middle';
+  LinkedList.InsertNext(LinkedList.Tail, LStrPtr);
+
+  New(LStrPtr);
+  LStrPtr^ := 'Tail';
+  LinkedList.InsertNext(LinkedList.Tail, LStrPtr);
+
+  LNode := LinkedList.Head;
+
+  While Assigned(LNode) do
+  begin
+       LStrPtr := LNode.Value;
+
+       WriteLn(LStrPtr^);
+       LNode := LNode.Next;
+  end;
+
+
 
 end.
 
